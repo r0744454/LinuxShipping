aaa5a3771603e9e990ac7efd989f0bfaf366210f
r0744454 <45202579+r0744454@users.noreply.github.com>
2019-12-15 12:31:02 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
   <title>Jupiler League</title>
 
   <meta charset="utf-8">
-  <meta name="author" content="RVolders">
+  <meta name="author" content="Fozkan">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -14,7 +14,7 @@
     <div class="container">
     <?php echo "<h1>Scoreboard Jupiler League</h1>"; ?>
     <?php
-    // Connexion et sélection de la base
+	    
     $conn = mysqli_connect('db', 'user', 'R1234-56', "myDb");
     $query = 'SELECT * From Person';
     $result = mysqli_query($conn, $query);
